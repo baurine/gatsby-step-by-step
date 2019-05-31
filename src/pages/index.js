c453c344d8ed13dfb9a8b5954f275670d0ad8ee8
baurine <2008.hbl@gmail.com>
2019-05-31 11:34:05 +0800

    step 10 - list all local files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,33 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>hello</h1>
-  </Layout>
-)
+export default ({ data }) => {
+  return (
+    <Layout>
+      <h1>Docs</h1>
+      {data.allFile.edges.map(({ node }) => (
+        <div key={node.relativePath}>
+          <div>name: {node.name}</div>
+          <div>size: {node.prettySize}</div>
+          <div>path: {node.relativePath}</div>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          ext
+          name
+        }
+      }
+    }
+  }
+`
