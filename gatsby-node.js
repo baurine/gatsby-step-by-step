791aeedb854b27807eed73e1fe5316c6a9124a75
baurine <2008.hbl@gmail.com>
2019-05-31 18:28:50 +0800

    step 14 - create page for each markdown file with empty content
    - createPages, createPage, template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path")
 // const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -8,12 +8,40 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     console.log(node)
     const parentNode = getNode(node.parent)
     console.log(parentNode)
-    const slug = path.join('/', parentNode.relativeDirectory, parentNode.name)
+    const slug = path.join("/", parentNode.relativeDirectory, parentNode.name)
     console.log(slug)
     createNodeField({
       node,
-      name: 'slug',
-      value: slug
+      name: "slug",
+      value: slug,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      console.log(JSON.stringify(result, null, 2))
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve("./src/templates/md-doc.js"),
+        })
+      })
+      resolve()
+    })
+  })
 }
---
+++ src/templates/md-doc.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => {
+  return (
+    <Layout>
+      <h1>markdown doc template</h1>
+      <p>doc content (to be filled...)</p>
+    </Layout>
+  )
+}
